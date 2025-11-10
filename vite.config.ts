c205b9e8b363b9a8e4e692b97a8c851478f1bff4
Devara Venkatrao <venkatrao00998@gmail.com>
2025-11-10 14:03:23 +0530

    UPDATE : updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    tsConfigPaths(),
-    tanstackStart({
-
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
     }),
-    nitroV2Plugin({
-      preset: 'vercel', 
+    tanstackStart({
+      srcDirectory: 'src',
     }),
     viteReact(),
   ],
